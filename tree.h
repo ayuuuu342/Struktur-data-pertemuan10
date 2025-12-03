125474b19a5566d701929b1c8cd300c05b654d93
Ayu Setyaning Tyas <ayusetyaningtyas13@gmail.com>
2025-12-03 22:13:44 +0700

    Add files via upload

---
+++ tree.h
@@ -0,0 +1,40 @@
+#ifndef TREE_H
+#define TREE_H
+
+struct Node { 
+    int data;
+    Node *left, *right;
+    int height;
+};
+
+class BinaryTree {
+private:
+    Node* root;
+
+    Node* insertNode(Node* node, int value);
+    Node* deleteNode(Node* node, int value);
+
+    Node getHeight(Node* node);
+    int getBalance(Node* node);
+
+    Node* rotateRight(Node* y);
+    Node* rotateLeft(Node* x);
+
+    Node* minValueNode(Node* node);
+
+    void inOrder(Node* node);
+    void preOrder(Node* node);
+    void postOrder(Node* node); 
+
+    public:
+    BinaryTree();
+    void insert(int value);
+    void deleteValue(int value);
+    void update(int oldVal, int newVal);
+
+    void inorder();
+    void preorder();
+    void postorder();
+};
+
+#endif 
